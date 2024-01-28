a7e7ecf3f0961500028707b5c2d8659f8b0a8fe9
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-01-28 18:03:49 -0500

    [feat ➕] compile ts 'function progress' to js

--- logical.js
+++ logical.js
@@ -1,51 +1,78 @@
-var TextoInput = document.querySelector('input[type="text"]');
-var BotonAnadir = document.getElementById('anexar');
-var BarraProgreso = document.getElementsByTagName('progress');
-var TextBarra = document.querySelector('p');
-var UlElement = document.querySelector('ul');
-var contador1 = 0;
-var AllCheck = document.getElementsByClassName('allcheck');
-var porcentaje = function () {
-    var contadorCheck = 0;
-    for (var i = 0; i < AllCheck.length; i++) {
+const TextoInput = document.querySelector('input[type="text"]');
+const BotonAnadir = document.getElementById('anexar');
+const BarraProgreso = document.querySelector('progress');
+const TextBarra = document.querySelector('p');
+const UlElement = document.querySelector('ul');
+let contador1 = 0;
+let AllCheck = document.getElementsByClassName('allcheck');
+let porcentaje = () => {
+    let barraValor = +BarraProgreso.getAttribute('value');
+    let aumentarValorBarra;
+    aumentarValorBarra = clearInterval(aumentarValorBarra);
+    let contadorCheck = 0;
+    for (let i = 0; i < AllCheck.length; i++) {
         if (AllCheck[i].checked === true) {
             contadorCheck++;
         }
     }
-    var CantidadChecked = Math.floor((contadorCheck / (AllCheck.length)) * 100);
+    let CantidadChecked = Math.floor((contadorCheck / (AllCheck.length)) * 100);
     TextBarra.textContent = CantidadChecked.toString() + '%';
+    let PorcentajeToNumber = Math.floor((contadorCheck / AllCheck.length) * 100);
+    if (barraValor < PorcentajeToNumber) {
+        aumentarValorBarra = setInterval(() => {
+            barraValor++;
+            BarraProgreso.setAttribute('value', barraValor.toString());
+            if (barraValor == PorcentajeToNumber) {
+                clearInterval(aumentarValorBarra);
+            }
+        }, 20);
+    }
+    else if (barraValor > PorcentajeToNumber) {
+        aumentarValorBarra = setInterval(() => {
+            barraValor--;
+            BarraProgreso.setAttribute('value', barraValor.toString());
+            if (barraValor == PorcentajeToNumber) {
+                clearInterval(aumentarValorBarra);
+            }
+        }, 20);
+    }
+    else {
+        clearInterval(aumentarValorBarra);
+    }
 };
-var FuncionPrincipal = function () {
+const FuncionPrincipal = () => {
     if (TextoInput.value.trim() !== "") {
-        var CrearLi_1 = document.createElement('li');
-        UlElement.insertAdjacentElement('afterbegin', CrearLi_1);
-        var DivPrincipal = document.createElement('div');
+        const CrearLi = document.createElement('li');
+        UlElement.insertAdjacentElement('afterbegin', CrearLi);
+        const DivPrincipal = document.createElement('div');
         DivPrincipal.classList.add("ContenedorPrincipal");
-        CrearLi_1.insertAdjacentElement('afterbegin', DivPrincipal);
-        var DivSegundario = document.createElement('div');
+        CrearLi.insertAdjacentElement('afterbegin', DivPrincipal);
+        const DivSegundario = document.createElement('div');
         DivSegundario.classList.add("check");
         DivPrincipal.insertAdjacentElement('afterbegin', DivSegundario);
-        var InputCheckbox = document.createElement('input');
+        const InputCheckbox = document.createElement('input');
         InputCheckbox.type = "checkbox";
         InputCheckbox.classList.add('allcheck');
         InputCheckbox.setAttribute('id', contador1.toString());
         DivSegundario.insertAdjacentElement('afterbegin', InputCheckbox);
-        var LabelInput = document.createElement('label');
+        const LabelInput = document.createElement('label');
         LabelInput.setAttribute('for', contador1.toString());
         LabelInput.textContent = TextoInput.value;
         InputCheckbox.insertAdjacentElement('afterend', LabelInput);
-        var DivSegundario2 = document.createElement('div');
+        const DivSegundario2 = document.createElement('div');
         DivSegundario2.classList.add('eliminar');
         DivPrincipal.insertAdjacentElement('beforeend', DivSegundario2);
-        var BotonEliminar = document.createElement('button');
+        const BotonEliminar = document.createElement('button');
         DivSegundario2.insertAdjacentElement('afterbegin', BotonEliminar);
-        var IconoBasura = document.createElement('i');
+        const IconoBasura = document.createElement('i');
         IconoBasura.classList.add('fa-solid');
         IconoBasura.classList.add('fa-trash');
         BotonEliminar.insertAdjacentElement('afterbegin', IconoBasura);
         TextoInput.value = "";
-        BotonEliminar.addEventListener('click', function () {
-            UlElement.removeChild(CrearLi_1);
+        // BotonEliminar.addEventListener('click', porcentaje)
+        BotonEliminar.addEventListener('click', () => {
+            UlElement.removeChild(CrearLi);
+            porcentaje();
         });
         InputCheckbox.addEventListener('click', porcentaje);
         contador1++;
@@ -53,9 +80,10 @@ var FuncionPrincipal = function () {
 };
 BotonAnadir.addEventListener('click', FuncionPrincipal);
 BotonAnadir.addEventListener('click', porcentaje);
-TextoInput.addEventListener('keydown', function (e) {
+TextoInput.addEventListener('keydown', (e) => {
     if (e.key == "Enter") {
         FuncionPrincipal();
         porcentaje();
     }
 });
+// console.log(BarraProgreso.getAttribute('value'))
