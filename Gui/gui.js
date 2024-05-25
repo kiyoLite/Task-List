b8c290432aab7d3c8a55aba929999aa8225fffe1
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-05-25 16:45:37 -0500

    [feat] Create a gui file where is in charge directly the web desing

--- Gui/gui.js
+++ Gui/gui.js
@@ -1,12 +1,6 @@
 import { AddTask } from "../Logic/AddTask.js";
 import { UpdateProgressbar } from "../Logic/UpdateProgressBar.js";
-
-const FormSubmitTask = document.querySelector("#TaskForm") as HTMLFormElement | null
-const Input = document.querySelector('input[type="text"]') as HTMLInputElement | null
-
-
+const FormSubmitTask = document.querySelector("#TaskForm");
+const Input = document.querySelector('input[type="text"]');
 FormSubmitTask?.addEventListener('submit', AddTask);
 FormSubmitTask?.addEventListener('submit', UpdateProgressbar);
-
-
-
