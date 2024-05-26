fd5e5a94ab4e8e51b319f6c36691331c463ec7da
Johan Steven <155318310+kiyoLite@users.noreply.github.com>
2024-05-26 13:05:20 -0500

    [feat] Add deltetask function inner addtask .
    Just create the task , link it with the function , in that way isn't necc
    esary use identifier to linked the function.

--- Logic/AddTask.js
+++ Logic/AddTask.js
@@ -1,23 +1,36 @@
+import { DeleteTask } from "./DeleteTask.js";
+import { UpdateProgressbar } from "./UpdateProgressBar.js";
 const TaskList = document.querySelector('ul');
 const Input = document.querySelector('input[type="text"]');
+const FormSubmitTask = document.querySelector("#TaskForm");
 let CheckboxID = 1;
-const MainFunction2 = () => {
+const AddTask = (event) => {
+    event.preventDefault();
     if (Input && TaskList && Input.value.trim() !== "") {
         const ListHtml = ` 
-        <li id="proof">
+        <li id="Task">
             <div class="ContenedorPrincipal">
                 <div class="check" >
-                    <input type="checkbox" id = ${CheckboxID.toString()}>
+                    <input type="checkbox" id = ${CheckboxID.toString()} class="allcheckbox">
                     <label for= ${CheckboxID.toString()} > ${Input.value} </label>
                 </div>
             <div class= "eliminar">
-                <button><i class= "fa-solid fa-trash" id="garbage"> </i> </button>
+                <button id="garbage">
+                    <img src="./images/Delete.svg" alt="garbage" id="ImageGarbage">
+                </button>
             </div>
             </div>
         </li>`;
         TaskList.insertAdjacentHTML("afterbegin", ListHtml);
         Input.value = "";
+        const DeleteTaskButton = document.getElementById("garbage");
+        const Task = document.getElementById("Task");
+        DeleteTaskButton?.addEventListener('click', () => {
+            DeleteTask(Task);
+        });
+        const TaskCheckBox = document.getElementById(CheckboxID.toString());
+        TaskCheckBox?.addEventListener('click', UpdateProgressbar);
         CheckboxID++;
     }
 };
-export { MainFunction2 };
+export { AddTask };
--- Logic/AddTask.ts
+++ Logic/AddTask.ts
@@ -1,25 +1,43 @@
+import { DeleteTask } from "./DeleteTask.js";
+import { UpdateProgressbar } from "./UpdateProgressBar.js";
+
 const TaskList = document.querySelector('ul') as HTMLUListElement | null
 const Input = document.querySelector('input[type="text"]') as HTMLInputElement | null
+const FormSubmitTask = document.querySelector("#TaskForm") as HTMLFormElement | null
 let CheckboxID = 1;
-const MainFunction2 = (): void => {
+const AddTask = (event: Event): void => {
+    event.preventDefault()
     if (Input && TaskList && Input.value.trim() !== "") {
+
+
         const ListHtml: string = ` 
-        <li id="proof">
+        <li id="Task">
             <div class="ContenedorPrincipal">
                 <div class="check" >
-                    <input type="checkbox" id = ${CheckboxID.toString()}>
+                    <input type="checkbox" id = ${CheckboxID.toString()} class="allcheckbox">
                     <label for= ${CheckboxID.toString()} > ${Input.value} </label>
                 </div>
             <div class= "eliminar">
-                <button><i class= "fa-solid fa-trash" id="garbage"> </i> </button>
+                <button id="garbage">
+                    <img src="./images/Delete.svg" alt="garbage" id="ImageGarbage">
+                </button>
             </div>
             </div>
         </li>`
         TaskList.insertAdjacentHTML("afterbegin", ListHtml);
         Input.value = ""
+        const DeleteTaskButton = document.getElementById("garbage")
+        const Task = document.getElementById("Task") as HTMLLIElement | null;
+
+        DeleteTaskButton?.addEventListener('click', () => {
+            DeleteTask(Task);
+        });
+
+        const TaskCheckBox = document.getElementById(CheckboxID.toString())
+        TaskCheckBox?.addEventListener('click', UpdateProgressbar);
 
         CheckboxID++
     }
 }
 
-export { MainFunction2 };
+export { AddTask };
